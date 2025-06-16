1cbfd4d6fa532dce686649aaf9178c10881c6eb2
lollomag <lorenzo.cadente@yahoo.it>
2025-06-16 20:30:41 +0200

    add cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,11 +1,49 @@
-export default [
-  'strapi::logger',
+// config/middlewares.js
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+            '*.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+            '*.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: "256mb",
+      jsonLimit: "256mb",
+      textLimit: "256mb",
+      formidable: {
+        maxFileSize: 100 * 1024 * 1024,
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,52 @@
-export default () => ({});
+// config/plugins.js
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {
+          folder: 'beverino-bike-festival',
+          use_filename: true,
+          unique_filename: false,
+          overwrite: false,
+          resource_type: 'auto',
+          transformation: [
+            {
+              quality: 'auto:best',
+              fetch_format: 'auto',
+            }
+          ],
+          eager: [
+            {
+              width: 150,
+              height: 150,
+              crop: 'thumb',
+              gravity: 'auto',
+              quality: 'auto',
+              fetch_format: 'auto'
+            },
+            {
+              width: 500,
+              crop: 'scale',
+              quality: 'auto',
+              fetch_format: 'auto'
+            },
+            {
+              width: 1000,
+              crop: 'scale', 
+              quality: 'auto',
+              fetch_format: 'auto'
+            }
+          ],
+          tags: ['beverino-bike-festival', `year-${new Date().getFullYear()}`]
+        },
+        delete: {},
+      },
+    },
+  },
+});
