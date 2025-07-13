88ad8550d27796655311ba5293e719fef2120040
lollomag <lorenzo.cadente@yahoo.it>
2025-07-13 13:04:52 +0200

    remove cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,53 +1,16 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-cloudinary', // Path completo per v5
+      provider: 'local',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        sizeLimit: 20 * 1024 * 1024, // 10MB massimo per file
       },
-      actionOptions: {
-        uploadStream: {
-          folder: 'beverino-bike-festival',
-          use_filename: true,
-          unique_filename: false,
-          overwrite: false,
-          resource_type: 'auto',
-          transformation: [
-            {
-              quality: 'auto:best',
-              fetch_format: 'auto',
-            }
-          ],
-          eager: [
-            {
-              width: 150,
-              height: 150,
-              crop: 'thumb',
-              gravity: 'auto',
-              quality: 'auto',
-              fetch_format: 'auto'
-            },
-            {
-              width: 500,
-              crop: 'scale',
-              quality: 'auto',
-              fetch_format: 'auto'
-            },
-            {
-              width: 1000,
-              crop: 'scale', 
-              quality: 'auto',
-              fetch_format: 'auto'
-            }
-          ],
-          tags: ['beverino-bike-festival', `year-${new Date().getFullYear()}`]
-        },
-        upload: {
-          folder: 'beverino-bike-festival',
-        },
-        delete: {},
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
       },
     },
   },
